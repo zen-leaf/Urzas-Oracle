d4072656909057e20f249fc4a4f2899a71278a9b
Trace98J <131201422+Trace98J@users.noreply.github.com>
2025-10-24 15:49:52 +0200

    java project init

---
+++ urzasoracle/src/main/java/com/mambocosmo/urzasoracle/UrzasoracleApplication.java
@@ -0,0 +1,13 @@
+package com.mambocosmo.urzasoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrzasoracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrzasoracleApplication.class, args);
+	}
+
+}
---
+++ urzasoracle/src/test/java/com/mambocosmo/urzasoracle/UrzasoracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mambocosmo.urzasoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrzasoracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
