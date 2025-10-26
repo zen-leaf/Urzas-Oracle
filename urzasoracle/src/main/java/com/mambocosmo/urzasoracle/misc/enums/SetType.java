a72585b052ed2af6c344f91fafdb52922ef51420
Trace98J <131201422+Trace98J@users.noreply.github.com>
2025-10-26 13:22:48 +0100

    populated SetType Enum

--- urzasoracle/src/main/java/com/mambocosmo/urzasoracle/misc/enums/SetType.java
+++ urzasoracle/src/main/java/com/mambocosmo/urzasoracle/misc/enums/SetType.java
@@ -1,5 +1,27 @@
 package com.mambocosmo.urzasoracle.misc.enums;
 
 public enum SetType {
-
+    starter,
+    archenemy,
+    treasure_chest,
+    masters,
+    box,
+    alchemy,
+    expansion,
+    token,
+    planechase,
+    promo,
+    core,
+    eternal,
+    draft_innovation,
+    commander,
+    spellbook,
+    from_the_vault,
+    memorabilia,
+    premium_deck,
+    vanguard,
+    funny,
+    duel_deck,
+    masterpiece,
+    arsenal
 }
