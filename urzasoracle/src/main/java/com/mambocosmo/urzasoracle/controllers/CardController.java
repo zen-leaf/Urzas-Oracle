097ea0831fbeae5fc44ae0993aa9d7ae66488c2d
Zen <glytch@hotmail.it>
2025-10-29 20:28:31 +0100

    removed prints to clean terminal feed

--- urzasoracle/src/main/java/com/mambocosmo/urzasoracle/controllers/CardController.java
+++ urzasoracle/src/main/java/com/mambocosmo/urzasoracle/controllers/CardController.java
@@ -27,6 +27,7 @@ public class CardController {
     public String getAllCards(Model model, @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
         Page<CardDTO> cardPage = getCARDSERVICE().getAllPaged(page, size);
+        //System.out.println("Card Page Size: " + cardPage.getContent().toString());
         model.addAttribute("lista", cardPage.getContent());
         model.addAttribute("currentPage", cardPage.getNumber());
         model.addAttribute("totalPages", cardPage.getTotalPages());
--- urzasoracle/src/main/java/com/mambocosmo/urzasoracle/converters/CardConverter.java
+++ urzasoracle/src/main/java/com/mambocosmo/urzasoracle/converters/CardConverter.java
@@ -28,7 +28,7 @@ public class CardConverter implements GenericConverter<Card, CardDTO> {
 
     @Override
     public CardDTO fromEToD(Card e) {
-        System.out.println("CARDFACELIST " + e.getCard_faces());
+        // System.out.println("CARDFACELIST " + e.getCard_faces());
         CardDTO dto = new CardDTO();
         dto.setId(e.getId());
         dto.setName(e.getName());
@@ -41,7 +41,7 @@ public class CardConverter implements GenericConverter<Card, CardDTO> {
             tempLegal.put(key.toString().toLowerCase(), value);
         });
         dto.setLegalities(tempLegal);
-        System.out.println("ARTISTREFF " + e.getArtistRef());
+        // System.out.println("ARTISTREFF " + e.getArtistRef());
         dto.setArtistRef(new ArrayList<>());
         if (e.getArtistRef() != null)
             e.getArtistRef().forEach(entry -> {
