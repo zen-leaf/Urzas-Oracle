0da44ed05ed8e0f80c3cc969fee8be22ad14e780
Zen <glytch@hotmail.it>
2025-10-29 20:27:51 +0100

    started work on generic query search

--- urzasoracle/src/main/java/com/mambocosmo/urzasoracle/services/CardService.java
+++ urzasoracle/src/main/java/com/mambocosmo/urzasoracle/services/CardService.java
@@ -84,7 +84,7 @@ public class CardService extends GenericService<Card, CardDTO, CardConverter, Ca
         try {
 
             cardData = mapper
-                    .readTree(new File("JAVITA148-ProjectWork\\urzasoracle\\src\\main\\resources\\json\\test.json"));
+                    .readTree(new File("urzasoracle/src/main/resources/json/test.json"));
             List<Card> cardList = new ArrayList<>();
             Long myTimer = System.nanoTime();
             System.out.println("start");
@@ -157,6 +157,12 @@ public class CardService extends GenericService<Card, CardDTO, CardConverter, Ca
         Pageable pageable = PageRequest.of(numeroPagina, dimensione);
         Page<Card> page = getREPOSITORY().findAll(pageable);
 
+        return page.map(card -> getCONVERTER().fromEToD(card));
+    }
+    public Page<CardDTO> searchInCard(String searchTerm, int numeroPagina, int dimensione) {
+        Pageable pageable = PageRequest.of(numeroPagina, dimensione);
+        Page<Card> page = getREPOSITORY()
+                .findByNameContainingIgnoreCase(searchTerm, pageable);
         return page.map(card -> getCONVERTER().fromEToD(card));
     }
 
