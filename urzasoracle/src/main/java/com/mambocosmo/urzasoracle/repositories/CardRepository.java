71561ade3ed5888f20772d37a2782953a46c8422
Zen <glytch@hotmail.it>
2025-10-31 14:26:44 +0100

    Implemented parametrized card search

--- urzasoracle/src/main/java/com/mambocosmo/urzasoracle/repositories/CardRepository.java
+++ urzasoracle/src/main/java/com/mambocosmo/urzasoracle/repositories/CardRepository.java
@@ -6,20 +6,20 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.mambocosmo.urzasoracle.entities.Card;
 
 @Repository
-public interface CardRepository extends JpaRepository<Card, UUID> {
+public interface CardRepository extends JpaRepository<Card, UUID>, JpaSpecificationExecutor<Card> {
 
     Page<Card> findByName(String name, Pageable pageable);
 
     List<Card> findByName(String name);
 
-    @Query(value="SELECT * FROM cards c ORDER BY RAND()",nativeQuery=true)
+    @Query(value = "SELECT * FROM cards c ORDER BY RAND()", nativeQuery = true)
     Page<Card> findRandomSubSet(int limit, Pageable pageable);
 
     // @Query("SELECT c FROM cards c ORDER BY RAND( ) LIMIT 100")
