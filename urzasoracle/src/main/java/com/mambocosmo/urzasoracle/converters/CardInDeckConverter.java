38b49f39a874a80d71ead52cf903396e217ae37a
SubashiLorenzo <159890822+SubashiLorenzo@users.noreply.github.com>
2025-10-31 16:17:02 +0100

    added frontend implementation for homepage and decks

--- urzasoracle/src/main/java/com/mambocosmo/urzasoracle/converters/CardInDeckConverter.java
+++ urzasoracle/src/main/java/com/mambocosmo/urzasoracle/converters/CardInDeckConverter.java
@@ -7,22 +7,21 @@ import com.mambocosmo.urzasoracle.entities.CardInDeck;
 @Component
 public class CardInDeckConverter {
     
-   public CardInDeckDTO toDTO(CardInDeck entity) {
-    if (entity == null) return null;
-    
-    CardInDeckDTO dto = new CardInDeckDTO();
-    dto.setCardId(entity.getId().getCard().getId());
-    dto.setCardName(entity.getId().getCard().getName());
-    dto.setTypeline(entity.getId().getCard().getType_line());
-    dto.setQuantity(entity.getQuantity());
-    
-    String imageUrl = null;
-    if (entity.getId().getCard().getImage_uris() != null) {
-        imageUrl = entity.getId().getCard().getImage_uris().get("normal");
+    public CardInDeckDTO toDTO(CardInDeck entity) {
+        if (entity == null) return null;
+        
+        CardInDeckDTO dto = new CardInDeckDTO();
+        dto.setCardId(entity.getId().getCard().getId());
+        dto.setCardName(entity.getId().getCard().getName());
+        dto.setTypeline(entity.getId().getCard().getType_line());
+        dto.setQuantity(entity.getQuantity());
+        
+        String imageUrl = null;
+        if (entity.getId().getCard().getImage_uris() != null) {
+            imageUrl = entity.getId().getCard().getImage_uris().get("normal");
+        }
+        dto.setImageUrl(imageUrl);
+        
+        return dto;
     }
-    dto.setImageUrl(imageUrl);
-    
-    return dto;
-}
-
 }
